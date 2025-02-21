99b4988082fe4c2880780d9aa85ef7357d4428a0
Alan Casal <alan.casal.dev@gmail.com>
2025-02-21 15:11:51 -0300

    Onboarding > Swiper - finished swiper

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -4,10 +4,17 @@ import { useRef, useState } from 'react';
 import { Image, Text, TouchableOpacity, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Swiper from 'react-native-swiper';
+import CustomButton from '@/components/CustomButton';
 
 const Onboarding = () => {
 	const swiperRef = useRef<Swiper>(null);
 	const [activeIndex, setActiveIndex] = useState(0);
+	const isLastSlide = activeIndex === onboarding.length - 1;
+
+	const handleNext = () => {
+		if (isLastSlide) router.replace('/(auth)/sign-up');
+		else swiperRef.current?.scrollBy(1);
+	};
 
 	return (
 		<SafeAreaView className='flex h-full items-center justify-between bg-white'>
@@ -37,6 +44,7 @@ const Onboarding = () => {
 						<Image
 							source={item.image}
 							className='w-full h-[300px]'
+							resizeMode='contain'
 						/>
 						<View className='flex flex-row items-center justify-center w-full mt-10'>
 							<Text className='text-black text-3xl font-bold mx-10 text-center'>
@@ -49,6 +57,12 @@ const Onboarding = () => {
 					</View>
 				))}
 			</Swiper>
+
+			<CustomButton
+				title={isLastSlide ? 'Get Started' : 'Next'}
+				onPress={handleNext}
+				className='w-11/12 mt-10'
+			/>
 		</SafeAreaView>
 	);
 };
