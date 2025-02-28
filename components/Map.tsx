f0232b03c4ad3c0a7b04c6f891b090506ef70d41
Alan Casal <alan.casal.dev@gmail.com>
2025-02-28 13:01:56 -0300

    Map - added styles for map redenring

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,10 +1,23 @@
-import { View, Text } from 'react-native';
+import { Text } from 'react-native';
+import MapView, { PROVIDER_DEFAULT } from 'react-native-maps';
+import { cssInterop } from 'nativewind';
+
+// Create a styled version of MapView
+const StyledMapView = cssInterop(MapView, { className: 'style' });
 
 const Map = () => {
 	return (
-		<View>
+		<StyledMapView
+			provider={PROVIDER_DEFAULT}
+			className='w-full h-full'
+			tintColor='black'
+			mapType='standard'
+			showsPointsOfInterest={false}
+			showsUserLocation
+			userInterfaceStyle='light'
+		>
 			<Text>Map</Text>
-		</View>
+		</StyledMapView>
 	);
 };
 
