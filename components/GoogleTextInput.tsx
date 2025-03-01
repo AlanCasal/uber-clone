cb271492969dbd44edbd3817d5c874f215b2f911
Alan Casal <alan.casal.dev@gmail.com>
2025-02-28 23:41:07 -0300

    Added google places lib & GooglePlacesAutocomplete

--- components/GoogleTextInput.tsx
+++ components/GoogleTextInput.tsx
@@ -1,5 +1,11 @@
+import 'react-native-get-random-values';
 import { GoogleInputProps } from '@/types/type';
-import { View, Text } from 'react-native';
+import React from 'react';
+import { View, Image } from 'react-native';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { icons } from '@/constants';
+
+const GOOGLE_API_KEY = process.env.EXPO_PUBLIC_GOOGLE_API_KEY;
 
 const GoogleTextInput = ({
 	icon,
@@ -12,7 +18,56 @@ const GoogleTextInput = ({
 		<View
 			className={`flex-row items-center justify-center z-50 rounded-xl mb-5 ${containerStyle}`}
 		>
-			<Text>Search</Text>
+			<GooglePlacesAutocomplete
+				placeholder='Where do you want to go?'
+				onPress={(data, details = null) => {
+					handlePress({
+						latitude: details?.geometry.location.lat || 0,
+						longitude: details?.geometry.location.lng || 0,
+						address: data.description,
+					});
+				}}
+				styles={{
+					textInputContainer: {
+						alignItems: 'center',
+						justifyContent: 'center',
+						borderRadius: 20,
+						marginHorizontal: 20,
+						shadowColor: '#d4d4d4',
+					},
+					textInput: {
+						backgroundColor: textInputBackgroundColor || '#fff',
+						fontSize: 16,
+						fontWeight: '600',
+						marginTop: 5,
+						width: '100%',
+						borderRadius: 200,
+					},
+					listView: {
+						backgroundColor: textInputBackgroundColor || '#fff',
+						top: 0,
+						width: '100%',
+						borderRadius: 10,
+						shadowColor: '#d4d4d4',
+						zIndex: 99,
+					},
+				}}
+				textInputProps={{
+					placeholderTextColor: '#c4c4c4',
+					placeholder: initialLocation ?? 'Where do you want to go?',
+				}}
+				renderLeftButton={() => (
+					<View className='justify-center items-center w-6 h-6'>
+						<Image
+							source={icon ? icon : icons.search}
+							className='w-6 h-6'
+							resizeMode='contain'
+						/>
+					</View>
+				)}
+				debounce={200}
+				query={{ key: GOOGLE_API_KEY, language: 'en' }}
+			/>
 		</View>
 	);
 };
